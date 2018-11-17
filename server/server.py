b9afed4ad73527606114b3bae230e0cfc17811b0
aleve030 <aleve030@student.liu.se>
2018-11-17 17:58:11 +0100

    server

--- server/server.py
+++ server/server.py
@@ -7,29 +7,30 @@ PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
 conn = None
 s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 s.bind((HOST, PORT))
-s.listen(1)
 
-print("waiting for connections")
-conn, addr = s.accept()
-print(addr, "connected")
-
-def update(data):
-    print(data)
-
+connected_to_server = False
+sent_count = 0
 clients = []
 class SimpleServer(WebSocket):
     initialized = False
     name = ""
 
     def handleMessage(self):
+        global connected_to_server
+        global sent_count
         if not self.initialized:
             self.name = self.data
             self.initialized = True
 
         # self.messages.append(self.data)
-        conn.sendall(self.data)
+        if connected_to_server:
+            try:
+                conn.sendall(self.data)
+                sent_count += 1
+            except:
+                connected_to_server = False
+                print("Disconnected")
         # update(self.data)
 
 
@@ -42,3 +43,24 @@ class SimpleServer(WebSocket):
        print(self.address, 'closed')
        for client in clients:
           client.sendMessage(self.address[0] + u' - disconnected')
+
+
+server = SimpleWebSocketServer('0.0.0.0', 8000, SimpleServer)
+ping_cnt = 0
+while True:
+    msg = None
+    if not connected_to_server:
+        print("waiting for connections")
+        s.listen(1)
+        connected_to_server = True
+        conn, addr = s.accept()
+        print(addr, "connected")
+    else:
+        if ping_cnt % 12 == 0:
+            try:
+                conn.sendall("ping||||||||")
+            except:
+                connected_to_server = False
+                print("Disconnected")
+    server.serveonce()
+    ping_cnt += 1
